4b60651f373589d7487cd815e46fe1a47a475d7b
SairovARG <pmrovira92@gmail.com>
2022-12-22 21:27:20 -0300

    update db config

--- src/models/connection.js
+++ src/models/connection.js
@@ -8,6 +8,7 @@ const config = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
+    insecureAuth : true
 }
 
 const conn = mysql.createConnection(config);
